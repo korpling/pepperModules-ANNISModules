682d27cb1ed44fe56dbbd2b0463b9453c0dd29c9
Thomas Krause <thomaskrause@posteo.de>
2015-02-11 15:05:49 +0100

    adjust testMapSorderRelation to new SaltSample

--- src/test/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/relannis/tests/Salt2RelANNISMapperTest.java
+++ src/test/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/relannis/tests/Salt2RelANNISMapperTest.java
@@ -555,7 +555,12 @@ public class Salt2RelANNISMapperTest
     SampleGenerator.createDialogue(getFixture().getSDocument());
 
     getFixture().setResourceURI(URI.createFileURI(tmpPath.getAbsolutePath()));
-
+  
+//    SDocumentGraphDOTWriter dot = new SDocumentGraphDOTWriter();
+//    dot.setSDocumentGraph(getFixture().getSDocument().getSDocumentGraph());
+//    dot.setOutputURI(URI.createFileURI("/tmp/testMapSOrderRelation.dot"));
+//    dot.save();
+    
     doMapping();
 
     assertFalse("There was no file to be compared in folder '" + testPath.
