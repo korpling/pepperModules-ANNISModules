d3b11815662deb861670e1cf8b923a309d7512cc
Thomas Krause <thomaskrause@posteo.de>
2015-02-11 15:09:31 +0100

    adjust testMapSSpan to new SaltSample

--- src/test/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/relannis/tests/Salt2RelANNISMapperTest.java
+++ src/test/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/relannis/tests/Salt2RelANNISMapperTest.java
@@ -350,6 +350,11 @@ public class Salt2RelANNISMapperTest
     SampleGenerator.
       createInformationStructureAnnotations(getFixture().getSDocument());
     getFixture().setResourceURI(URI.createFileURI(tmpPath.getAbsolutePath()));
+      
+//    SDocumentGraphDOTWriter dot = new SDocumentGraphDOTWriter();
+//    dot.setSDocumentGraph(getFixture().getSDocument().getSDocumentGraph());
+//    dot.setOutputURI(URI.createFileURI("/tmp/testMapSSpans.dot"));
+//    dot.save();
 
     doMapping();
 
@@ -555,11 +560,6 @@ public class Salt2RelANNISMapperTest
     SampleGenerator.createDialogue(getFixture().getSDocument());
 
     getFixture().setResourceURI(URI.createFileURI(tmpPath.getAbsolutePath()));
-  
-//    SDocumentGraphDOTWriter dot = new SDocumentGraphDOTWriter();
-//    dot.setSDocumentGraph(getFixture().getSDocument().getSDocumentGraph());
-//    dot.setOutputURI(URI.createFileURI("/tmp/testMapSOrderRelation.dot"));
-//    dot.save();
     
     doMapping();
 
